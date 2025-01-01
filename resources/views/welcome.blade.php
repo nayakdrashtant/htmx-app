6989f08b9e38d9d8b9feeddb3094d57f6eb27e0f
Drashtant Nayak <drashtantnayak@dal.ca>
2025-01-01 16:57:29 -0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+    </head>
+    <body class="antialiased bg-slate-700">
+       <div class="flex justify-center px-10 py-10 w-full">
+           <div class="flex w-1/2 space-x-4 justify-center">
+                <button
+                    hx-get="/hello"
+                    class="bg-indigo-600 text-white p-4 text-lg font-semibold rounded-md">
+                    Load Content
+                </button>
+           </div>
+       </div>
+    </body>
+</html>
